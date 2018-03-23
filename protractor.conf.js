9a85faadd5284311cbc0deb429ae95a64b5e6db7
Pavel Sharapov <p.sharapov1@gmail.com>
2018-03-23 16:07:29 +0200

    test git again

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,17 +1,13 @@
 // All_elements_is_present on page
 exports.config = {
 
-    framework: 'jasmine2',
+    framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    // specs: ['./e2e/Header-elements-present.component.spec.js', './e2e/Body-elements-present-spec.js','./e2e/Footer-elements-present-spec.js','./e2e/Login-spec.js'],
-    // specs: ['./e2e/Footer-elements-present-spec.js'],
     // specs: ['./e2e/welcomePageElementsPresent-spec.js','./e2e/registrationViaEmail-spec.js','./e2e/loginViaUserName-spec.js'],
-    //  specs: ['./e2e/RegistrationViaEmail-spec.js'],
-    //  specs: ['./e2e/Welcome-page-elements-present-spec.js'],
-     specs: ['./e2e/loginViaUserName-spec.js'],
-    restartBrowserBetweenTests: false,
-
-      capabilities: {
+    //  specs: ['./e2e/welcomePageElementsPresent-spec.js'],
+    //  specs: ['./e2e/registrationViaEmail-spec.js'],
+    //  specs: ['./e2e/loginViaUserName-spec.js'],
+    capabilities: {
         browserName: 'chrome'
         /* chromeOptions: {
 
